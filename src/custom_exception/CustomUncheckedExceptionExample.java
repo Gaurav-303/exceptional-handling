f1d8f3c4c3f2124b0eaccbbdfeb073d4793f4075
Gaurav-303 <gaurav134mishra@gmail.com>
2025-09-03 11:43:51 +0530

    custom unchecked exception example

--- src/custom_exception/CustomUncheckedExceptionExample.java
+++ src/custom_exception/CustomUncheckedExceptionExample.java
@@ -1,4 +1,27 @@
 package custom_exception;
 
+
+import java.util.Scanner;
+
 public class CustomUncheckedExceptionExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter an integer: ");
+            int number = sc.nextInt();
+
+
+            if (number < 0) {
+                throw new NegativeNumberException("Negative numbers are not allowed: " + number);
+            }
+
+            System.out.println("You entered: " + number);
+
+        } catch (NegativeNumberException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+
+        sc.close();
+    }
 }
